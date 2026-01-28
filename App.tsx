1388f600803659b81a6c96ef4ca530fd3c5fe675
Froilán Olesti Casas <frolesti4@gmail.com>
2026-01-28 09:24:45 +0100

    chore: setup auto-proxy start script and suppress warnings

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { LogBox } from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
 import { AuthProvider } from './src/context/AuthContext';
+
+LogBox.ignoreLogs(['props.pointerEvents is deprecated']);
 
 export default function App() {
   return (
